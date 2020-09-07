032462c58fd018892dfe0e25fb63e58e7ad6127b
anthoniomadu <31852341+anthoniomadu@users.noreply.github.com>
2020-09-07 12:17:38 +0100

    user seperate

--- my-keystone-app/index.js
+++ my-keystone-app/index.js
@@ -21,55 +21,18 @@ const keystone = new Keystone({
 const admin = new AdminUIApp();
 // const admin = new AdminUIApp({ authStrategy });
 
-// Access control functions
-const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
-const userOwnsItem = ({ authentication: { item: user } }) => {
-  if (!user) {
-    return false;
-  }
-
-  // Instead of a boolean, you can return a GraphQL query:
-  // https://www.keystonejs.com/api/access-control#graphqlwhere
-  return { id: user.id };
-};
-
-const userIsAdminOrOwner = auth => {
-  const isAdmin = access.userIsAdmin(auth);
-  const isOwner = access.userOwnsItem(auth);
-  return isAdmin ? isAdmin : isOwner;
-};
-
-const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
 
 
-keystone.createList('User', {
-  fields: {
-    name: { type: Text },
-    email: {
-      type: Text,
-      isUnique: true,
-    },
-    isAdmin: {
-      type: Checkbox,
-      // Field-level access controls
-      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
-      access: {
-        update: access.userIsAdmin,
-      },
-    },
-    password: {
-      type: Password,
-    },
-  },
-  // List-level access controls
-  access: {
-    read: access.userIsAdminOrOwner,
-    update: access.userIsAdminOrOwner,
-    create: access.userIsAdmin,
-    delete: access.userIsAdmin,
-    auth: true,
-  },
-});
+
+
+const { User } = require("./models/user");
+// const { Role } = require("./role");
+// const { Post } = require("./post");
+
+keystone.createList("User", User);
+// keystone.createList("Role", Role);
+// keystone.createList("Post", Post);
+
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
@@ -88,7 +51,7 @@ module.exports = {
       adminPath: '/admin',
       name: PROJECT_NAME,
       enableDefaultRoute: true,
-      authStrategy,
+      // authStrategy,
     }),
   ],
 };
