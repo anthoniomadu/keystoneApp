a9c834346ddd41d0deee55f8fc674cb308b2dfc7
anthoniomadu <31852341+anthoniomadu@users.noreply.github.com>
2020-09-08 10:32:37 +0100

    Stripped 1

--- my-keystone-app/index.js
+++ my-keystone-app/index.js
@@ -6,10 +6,8 @@ const { AdminUIApp } = require('@keystonejs/app-admin-ui');
 const initialiseData = require('./initial-data');
 
 const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');
-const PROJECT_NAME = 'my_keystone_app';
-const adapterConfig = { knexOptions: { connection: 'postgres://postgres:1234@localhost:5432/my_keystone_project' } };
-
-
+const PROJECT_NAME = 'lu-app';
+const adapterConfig = { knexOptions: { connection: 'postgres://postgres:1234@localhost:5432/lu_app' } };
 
 
 const keystone = new Keystone({
@@ -17,30 +15,58 @@ const keystone = new Keystone({
   onConnect: process.env.CREATE_TABLES !== 'true' && initialiseData,
 });
 
+// Access control functions
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userOwnsItem = ({ authentication: { item: user } }) => {
+  if (!user) {
+    return false;
+  }
 
-const admin = new AdminUIApp();
-// const admin = new AdminUIApp({ authStrategy });
+  // Instead of a boolean, you can return a GraphQL query:
+  // https://www.keystonejs.com/api/access-control#graphqlwhere
+  return { id: user.id };
+};
 
+const userIsAdminOrOwner = auth => {
+  const isAdmin = access.userIsAdmin(auth);
+  const isOwner = access.userOwnsItem(auth);
+  return isAdmin ? isAdmin : isOwner;
+};
 
+const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
 
-
-
-const { User } = require("./models/user");
-// const { Role } = require("./role");
-// const { Post } = require("./post");
-
-keystone.createList("User", User);
-// keystone.createList("Role", Role);
-// keystone.createList("Post", Post);
-
+keystone.createList('User', {
+  fields: {
+    name: { type: Text },
+    email: {
+      type: Text,
+      isUnique: true,
+    },
+    isAdmin: {
+      type: Checkbox,
+      // Field-level access controls
+      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+      access: {
+        update: access.userIsAdmin,
+      },
+    },
+    password: {
+      type: Password,
+    },
+  },
+  // List-level access controls
+  access: {
+    read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
   list: 'User',
-  config: {
-    identityField: 'username', // default: 'email'
-    secretField: 'password', // default: 'password'
-  },
 });
 
 module.exports = {
@@ -48,38 +74,9 @@ module.exports = {
   apps: [
     new GraphQLApp(),
     new AdminUIApp({
-      adminPath: '/admin',
       name: PROJECT_NAME,
       enableDefaultRoute: true,
-      // authStrategy,
+      authStrategy,
     }),
   ],
 };
-
-
-// const { Keystone } = require('@keystonejs/keystone');
-// const { Text, Password } = require('@keystonejs/fields');
-// const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
-// const { AdminUIApp } = require('@keystonejs/app-admin-ui');
-
-// const keystone = new Keystone();
-
-// keystone.createList('User', {
-//   fields: {
-//     username: { type: Text },
-//     password: { type: Password },
-//   },
-// });
-
-// const authStrategy = keystone.createAuthStrategy({
-//   type: PasswordAuthStrategy,
-//   list: 'User',
-//   config: {
-//     identityField: 'username', // default: 'email'
-//     secretField: 'password', // default: 'password'
-//   },
-// });
-
-// // Enable Admin UI login by adding the authentication strategy
-// // const admin = new AdminUIApp({ authStrategy });
-// const admin = new AdminUIApp();
